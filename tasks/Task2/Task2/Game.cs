dbf8648403aa453cde4273b84588aa26cd1ba6d1
ic16b009 <ic16b009@technikum-wien.at>
2017-03-05 19:34:13 +0100

    add project Task2

---
+++ tasks/Task2/Task2/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Game
+    {
+        private decimal gprice;
+
+
+        /// <summary>
+        /// Creates a new book object.
+        /// </summary>
+        /// <param name="title">Title must not be empty.</param>
+        /// <param name="isbn">International Standard Book Number.</param>
+        /// <param name="price">Price must not be negative.</param>
+        /// 
+
+        public Game(string name, string genre, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Title must not be empty.", nameof(name));
+            if (string.IsNullOrWhiteSpace(genre)) throw new ArgumentException("ISBN must not be empty.", nameof(genre));
+
+            Name = name;
+            Genre = genre;
+            UpdatePrice(price);
+        }
+
+ 
+
+        private void UpdatePrice(decimal price)
+        {
+            try
+            {
+
+                if (price > 0 && price < 9999999)
+                gprice = price;
+            }
+            catch
+            {
+                throw new ArgumentException("Price must be realistic.", nameof(price));
+            }
+
+        }
+
+        public string Name { get; } //Videogame name
+
+        public string Genre { get; }
+
+        public decimal getPrice => gprice;
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new[]
+            {
+                new Game("CSGO", "Egoshooter", 9.90m),
+                new Game("Dirt 2", "Race", 12.90m),
+                new Game("Farmsimulator", "Simulation", -20.90m),
+                new Game("Flight-X", "Simulation", 29.90m),
+                new Game("Trainsimulator", "Simulation", 11.20m),
+                new Game("Goatsimulator", "Simulation", 4.30m),
+            };
+
+
+            foreach (var g in games)
+            {
+                Console.WriteLine("{0},\t{1},\t{2,8:0.00}", g.Name, g.Genre, g.getPrice);
+            }
+
+
+
+
+            Console.WriteLine("\n\n----- Genre Filter: -----");
+            foreach (var g in games)
+            {
+                if(g.Genre=="Simulation")
+                Console.WriteLine("{0},\t{1},\t{2,8:0.00}", g.Name, g.Genre, g.getPrice);
+            }
+
+            Console.WriteLine("\n\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+    }
+
+}
