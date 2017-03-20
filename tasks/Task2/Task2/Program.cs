ce178bfeac60c5968b096720c8a5b790dad93b0c
ic16b009 <ic16b009@technikum-wien.at>
2017-03-20 22:13:09 +0100

    modded for Task4

--- tasks/Task2/Task2/Program.cs
+++ tasks/Task2/Task2/Program.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Task2
 {
+    [TestFixture]
+
     class Program
     {
+        [Test]
         static void Main(string[] args)
         {
             var vgames = new[]
             {
                 new Videogame("CSGO", "Egoshooter", 9.90m, 3.2),
                 new Videogame("CSGO", "Egoshooter", 9.90m, 3.2)
+                 
+                //Assert.IsTrue(vgames[1]!=null);
 
             };
 
